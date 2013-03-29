ea98a8bd4bcb23fe8bf3d20ed8e14f2f8db2bb04
Andrey Konovalov <adech.fo@gmail.com>
2013-03-30 00:59:00 +0400

    Issue #14 resolved.

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-    $(".task_date").datepicker();
-    $(".task_date").datepicker("option", "dateFormat", "dd.mm.yy");
-    $(".task_date").datepicker("option", "firstDay", 1);
+    $('.task_date').datepicker();
+    $('.task_date').datepicker('option', 'dateFormat', 'dd.mm.yy');
+    $('.task_date').datepicker('option', 'firstDay', 1);
+    $('.add_task').hide()
  });
  
- function SwitchVisibility(id) {
+function SwitchVisibility(id) {
     if (document.getElementById) {
         if (document.getElementById(id).style.display == 'block') {
             document.getElementById(id).style.display = 'none';
