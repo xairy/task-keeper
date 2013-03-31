7187a2565f66fb7f9af126841db37d4252199eec
Andrey Konovalov <adech.fo@gmail.com>
2013-03-31 12:25:05 +0400

    Refactoring.

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,17 @@ $(document).ready(function() {
     $('.task_date').datepicker();
     $('.task_date').datepicker('option', 'dateFormat', 'dd.mm.yy');
     $('.task_date').datepicker('option', 'firstDay', 1);
-    $('.add_task').hide()
- });
- 
-function SwitchVisibility(id) {
-    if (document.getElementById) {
-        if (document.getElementById(id).style.display == 'block') {
-            document.getElementById(id).style.display = 'none';
-        } else {
-            document.getElementById(id).style.display = 'block';
-        }
-    }
+    SwitchToViewMode();
+});
+
+function SwitchToViewMode() {
+    $('.add_task').hide();
+}
+
+function SwitchToEditMode() {
+    $('.add_task').show();
+}
+
+function ToggleID(id) {
+    $('#' + id).toggle();
 }
